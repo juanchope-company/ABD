bbbd986d99e177d624b4bf9e86582902480f9ee6
Juanchope <Juanchope@Juanchope>
2021-03-13 22:16:14 -0500

    Se re estructuro mejor el modelo de la version 2 se centraliza los objectos -Tabla -Objeto_Tabla

---
+++ src/com/Cifrado/Codificacion.java
@@ -0,0 +1,32 @@
+package com.Cifrado;
+
+import java.util.Base64;
+
+/**
+ *
+ * @author Juanchope
+ */
+public class Codificacion {
+        
+/**
+ * 
+ * @see  Convierte un arreglo de Bytes unicode en un texto plano con formato Base64
+ * @param cifrado Arreglo de bytes a convertir a texto plano legible
+ * @return Devuelve el texto plano legible en Base64
+ * 
+ */
+    public static String codificar(byte[] cifrado) {
+        return Base64.getEncoder().encodeToString(cifrado);
+    }
+/**
+ * 
+ * @see Convierte un texto plano con formato Base64 en un arreglo de Bytes unicode
+ * @param textocifrado Texto plano con formato Base64
+ * @return Arreglo de Bytes unicode
+ * 
+ */
+
+    public static byte[] decodificar(String textocifrado) {
+        return Base64.getDecoder().decode(textocifrado);
+    }
+}
---
+++ src/com/Cifrado/Hash.java
@@ -0,0 +1,53 @@
+package com.Cifrado;
+
+import static com.Cifrado.Codificacion.codificar;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ *
+ * @author Juanchope
+ * @version 1.0
+ * 
+ */
+public class Hash {
+    
+/**
+ * 
+ * @see Metodo para generar el hash unico de la contraseña 
+ * @param texto Texto a generar el hash
+ * @return Devuelve el hash del mensaje
+ * 2048 bit keys should be secure until 2030 - https://web.archive.org/web/20170417095741/https://www.emc.com/emc-plus/rsa-labs/historical/twirl-and-rsa-key-size.htm
+ */
+    public static String HashSHA512(String texto){//256, 512
+        try {
+            //Ingresa el mensaje en un objecto de cifrado de mensajes
+            MessageDigest digestordeMensages = MessageDigest.getInstance("SHA-" + 512);
+            
+            //cifra os bytes del texto en formato utf_8
+            return  codificar(digestordeMensages.digest(texto.getBytes(StandardCharsets.UTF_8)));
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+    
+    private static final int            SECURE_TOKEN_LENGTH = 32;
+    private static final String         KEY          = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final SecureRandom   RANDOM              = new SecureRandom();
+
+/**
+ * Generate the next secure random token in the series.
+ * @return Devuelve un nuevo token
+ */
+    public static String GenerarToken() {    
+        char[] symbols = KEY.toCharArray(),
+        buf = new char[SECURE_TOKEN_LENGTH];
+        for (int idx = 0; idx < buf.length; ++idx)
+            buf[idx] = symbols[RANDOM.nextInt(symbols.length)];
+        return new  String(buf);
+    }
+}
+
